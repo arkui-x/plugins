57c851925fd546e35dbddfb0e025bd8203c182b3
quguiren@huawei.com <quguiren@huawei.com>
2023-05-05 15:48:56 +0800

    Add the ios code of ohos.net.connection cross platform
    Signed-off-by: quguiren@huawei.com <quguiren@huawei.com>

---
+++ net/connection/ios/net_status_listen.mm
@@ -0,0 +1,142 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#import <arpa/inet.h>
+#import <ifaddrs.h>
+#import <netdb.h>
+#import <netinet/in.h>
+#import <sys/socket.h>
+#import <CoreFoundation/CoreFoundation.h>
+
+#import "net_status_listen.h"
+#import "net_conn_client_adapter.h"
+
+static void ReachabilityCallback(SCNetworkReachabilityRef target, SCNetworkReachabilityFlags flags, void* info)
+{
+    NSLog(@"ReachabilityCallback");
+#pragma unused (target, flags)
+    NSCAssert(info != NULL, @"info was NULL in ReachabilityCallback");
+    NSCAssert([(__bridge NSObject*) info isKindOfClass: [NetStatusListen class]],
+        @"info was wrong class in ReachabilityCallback");
+    NetStatusListen* noteObject = (__bridge NetStatusListen *)info;
+    OHOS::Plugin::NetConnClientAdapter::NotifyNetworkStatusChanged(noteObject);
+}
+
+#pragma mark - NetStatusListen implementation
+
+@implementation NetStatusListen
+{
+    SCNetworkReachabilityRef _reachabilityRef;
+}
+
++ (instancetype)reachabilityWithAddress:(const struct sockaddr *)hostAddress
+{
+    NetStatusListen* instance = NULL;
+    SCNetworkReachabilityRef reachability = SCNetworkReachabilityCreateWithAddress(kCFAllocatorDefault, hostAddress);
+    if (reachability != NULL) {
+        instance = [[self alloc] init];
+        if (instance != NULL) {
+            instance->_reachabilityRef = reachability;
+        } else {
+            CFRelease(reachability);
+        }
+    }
+    return instance;
+}
+
++ (instancetype)reachabilityForInternetConnection
+{
+    struct sockaddr_in zeroAddress;
+    bzero(&zeroAddress, sizeof(zeroAddress));
+    zeroAddress.sin_len = sizeof(zeroAddress);
+    zeroAddress.sin_family = AF_INET;
+
+    return [self reachabilityWithAddress: (const struct sockaddr *) &zeroAddress];
+}
+
+#pragma mark - Start and stop monitor
+
+- (BOOL)startMonitor
+{
+    BOOL retCode = NO;
+    SCNetworkReachabilityContext reachAbilityContext = {0, (__bridge void *)(self), NULL, NULL, NULL};
+
+    if (SCNetworkReachabilitySetCallback(_reachabilityRef, ReachabilityCallback, &reachAbilityContext)) {
+        if (SCNetworkReachabilityScheduleWithRunLoop(_reachabilityRef, CFRunLoopGetCurrent(), kCFRunLoopDefaultMode)) {
+            retCode = YES;
+        }
+    }
+
+    return retCode;
+}
+
+- (void)stopMonitor
+{
+    if (_reachabilityRef != NULL) {
+        SCNetworkReachabilityUnscheduleFromRunLoop(_reachabilityRef, CFRunLoopGetCurrent(), kCFRunLoopDefaultMode);
+    }
+}
+
+- (void)dealloc
+{
+    [self stopMonitor];
+    if (_reachabilityRef != NULL) {
+        CFRelease(_reachabilityRef);
+    }
+}
+
+#pragma mark - Network Flag Handling
+
+- (NetworkStatus)getNetworkStatusForFlags:(SCNetworkReachabilityFlags)flags
+{
+    NetworkStatus retCode = NotReachable;
+
+    if ((flags & kSCNetworkReachabilityFlagsReachable) == 0) {
+        // The target host is not reachable.
+        return NotReachable;
+    }
+
+    if ((flags & kSCNetworkReachabilityFlagsConnectionRequired) == 0) {
+        // The target host is reachable via wifi.
+        retCode = ReachableViaWiFi;
+    }
+
+    if ((((flags & kSCNetworkReachabilityFlagsConnectionOnDemand ) != 0) ||
+        (flags & kSCNetworkReachabilityFlagsConnectionOnTraffic) != 0)) {
+        if ((flags & kSCNetworkReachabilityFlagsInterventionRequired) == 0) {
+            // The target host is reachable via wifi.
+            retCode = ReachableViaWiFi;
+        }
+    }
+
+    if ((flags & kSCNetworkReachabilityFlagsIsWWAN) == kSCNetworkReachabilityFlagsIsWWAN) {
+        // The target host is reachable via wwan.
+        retCode = ReachableViaWWAN;
+    }
+
+    return retCode;
+}
+
+- (NetworkStatus)getCurrentReachabilityStatus
+{
+    NSAssert(_reachabilityRef != NULL, @"getCurrentReachabilityStatus called with NULL SCNetworkReachabilityRef");
+    NetworkStatus status = NotReachable;
+    SCNetworkReachabilityFlags flags;
+    if (SCNetworkReachabilityGetFlags(_reachabilityRef, &flags)) {
+        status = [self getNetworkStatusForFlags:flags];
+    }
+    return status;
+}
+@end
