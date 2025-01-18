a25fde0e1c234bafa5ff9baa7d130fc93ab1edfd
quguiren <quguiren@huawei.com>
2025-01-18 14:08:49 +0800

    add webview
    Signed-off-by: quguiren <quguiren@huawei.com>

--- web/webview/ios/web_download_manager_ios.cpp
+++ web/webview/ios/web_download_manager_ios.cpp
@@ -22,25 +22,26 @@ WebDownloadDelegate* GetDownloadDelegate(int32_t webId) {
     WebDownloadDelegate* webDownloadDelegate = WebDownloadManager::GetWebDownloadDelegate(webId);
     if (!webDownloadDelegate) {
         LOGW("can't found delegate for nweb.");
-        webDownloadDelegate = WebDownloadManager::GetWebDownloadDefaultDelegate();
-        if (!webDownloadDelegate) {
-            return nullptr;
-        }
+        return nullptr;
     }
     return webDownloadDelegate;
 }
 
-void UpdateDownloadItem(int32_t webId, WebDownloadItem* webDownloadItem) {
+void UpdateDownloadItem(int32_t webId, bool isUpdate, WebDownloadItem* webDownloadItem) {
     WebDownloadDelegate* webDownloadDelegate = GetDownloadDelegate(webId);
     if (webDownloadDelegate) {
-        webDownloadDelegate->DownloadDidUpdate(webDownloadItem);
+        if (isUpdate) {
+            webDownloadDelegate->DownloadDidUpdate(webDownloadItem);
+        } else {
+            webDownloadDelegate->DownloadDidFail(webDownloadItem);
+        }
     }
 }
 
 void AssignWebDownloadManagerByObject(int32_t webId, WebDownloadItem* webDownloadItem,
     const std::shared_ptr<AceWebDownloadImpl> webDownloadImpl)
 {
-    if(webDownloadItem == nullptr) {
+    if (webDownloadItem == nullptr) {
         LOGE("webDownloadItem or object is nullptr can't assign");
         return;
     }
@@ -50,7 +51,7 @@ void AssignWebDownloadManagerByObject(int32_t webId, WebDownloadItem* webDownloa
     webDownloadItem->percentComplete = webDownloadImpl->GetPercentComplete();
     webDownloadItem->totalBytes = webDownloadImpl->GetTotalBytes();
     webDownloadItem->state = static_cast<PluginWebDownloadItemState>(webDownloadImpl->GetState());
-    webDownloadItem->lastErrorCode = 0;
+    webDownloadItem->lastErrorCode = webDownloadImpl->GetLastErrorCode();
     webDownloadItem->method = webDownloadImpl->GetMethod();
     webDownloadItem->mimeType = webDownloadImpl->GetMimeType();
     webDownloadItem->url = webDownloadImpl->GetUrl();
@@ -61,24 +62,24 @@ void AssignWebDownloadManagerByObject(int32_t webId, WebDownloadItem* webDownloa
     webDownloadItem->beforeCallback = [webId, guid, webDownloadItem](const std::string path)->void {
         webDownloadItemStartOC(webId, guid, path);
         webDownloadItem->state = PluginWebDownloadItemState::PENDING;
-        UpdateDownloadItem(webId, webDownloadItem);
+        UpdateDownloadItem(webId, true, webDownloadItem);
     };
     webDownloadItem->downloadCancelCallback = [webId, guid, webDownloadItem]()->void {
         webDownloadItemCancelOC(webId, guid);
         webDownloadItem->lastErrorCode = download::DOWNLOAD_INTERRUPT_REASON_USER_CANCELED;
         webDownloadItem->state = PluginWebDownloadItemState::CANCELED;
-        UpdateDownloadItem(webId, webDownloadItem);
+        UpdateDownloadItem(webId, false, webDownloadItem);
     };
     webDownloadItem->downloadPauseCallback = [webId, guid, webDownloadItem]()->int {
         webDownloadItemPauseOC(webId, guid);
         webDownloadItem->state = PluginWebDownloadItemState::PAUSED;
-        UpdateDownloadItem(webId, webDownloadItem);
+        UpdateDownloadItem(webId, true, webDownloadItem);
         return 0;
     };
     webDownloadItem->downloadResumeCallback = [webId, guid, webDownloadItem]()->int {
         webDownloadItemResumeOC(webId, guid);
         webDownloadItem->state = PluginWebDownloadItemState::PENDING;
-        UpdateDownloadItem(webId, webDownloadItem);
+        UpdateDownloadItem(webId, true, webDownloadItem);
         return 0;
     };
 }
