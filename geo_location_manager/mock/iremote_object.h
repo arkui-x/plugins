4fb26e24f6fa47f45fb9864f00f905314e3f6cb8
libo123456 <libo512@huawei-partners.com>
2025-11-13 18:12:39 +0800

    update: 更新文件 iremote_object.h
    Signed-off-by: libo123456 <libo512@huawei-partners.com>

--- geo_location_manager/mock/iremote_object.h
+++ geo_location_manager/mock/iremote_object.h
@@ -80,7 +80,7 @@ public:
     std::u16string GetObjectDescriptor() const;
 
 protected:
-    explicit IRemoteObject(std::u16string descriptor = nullptr);
+    explicit IRemoteObject(std::u16string descriptor = u"");
     virtual ~IRemoteObject() = default;
 };
 } // namespace OHOS
