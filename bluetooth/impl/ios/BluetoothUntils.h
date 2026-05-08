a35b685a28680750332668bee757bdf8a602fe1a
liuliuliu001 <liubin399@huawei.com>
2026-05-08 18:35:24 +0800

    蓝牙 Characteristic 相关接口实现
    Signed-off-by: liuliuliu001 <liubin399@huawei.com>

--- bluetooth/impl/ios/BluetoothUntils.h
+++ bluetooth/impl/ios/BluetoothUntils.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2024 Huawei Device Co., Ltd.
+ * Copyright (C) 2024-2026 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -23,6 +23,8 @@
 
 + (id)GetValueWithData:(NSData*)data uuid:(NSString*)strUUID;
 + (NSString*)GetSystemStringUUIDKey:(NSString*)string;
++ (NSString*)NormalizeBluetoothUuid:(NSString*)uuid;
++ (BOOL)IsSameBluetoothUuid:(NSString*)left right:(NSString*)right;
 
 @end
 
